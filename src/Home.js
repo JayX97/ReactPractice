b47fb3bad4455c033f5d9a74e4930e168c63a6ff
Jason Fernandez <jason.fernandezx@yahoo.com>
2024-07-28 02:32:44 -0400

    Learned about click handlers, click functions, and click events in React Apps

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (event) => {
+        console.log('Hello ninjas!', event);
+    };
+
+    const handleClickAgain = (name, event) => {
+        console.log('Hello ' + name, event.target);
+    };
+
     return (
         <div className='home'>
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(event) => handleClickAgain('Mario', event)}>Click me again</button>
         </div>
     );
 }
