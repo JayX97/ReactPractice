1c3049d55acce9028c9b838feb0a7cd6486cb109
JayX97 <jason.fernandezx@yahoo.com>
2024-08-06 03:50:40 -0400

    Learned about useEffect hook and useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
@@ -9,14 +9,23 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('Mario');
+
     const handleDelete = (id) => { // avoid mutating original array
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     };
 
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
+
     return (
         <div className='home'>
             <BlogList blogs={blogs} title='All blogs!' handleDelete={handleDelete} /> {/* reusable component with prop */}
+            <button onClick={() => setName('Luigi')}>change name</button>
+            <p>{ name }</p>
         </div>
     );
 }
